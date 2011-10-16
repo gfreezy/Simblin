b8463a4bc0d0bfd06f919dd12b2bb32c7edb29e2
gfreezy <gfreezy@gmail.com>
2011-10-16 06:29:45 +0000

    initdb with mysql

--- initdb.py
+++ initdb.py
@@ -1,13 +1,15 @@
 """Creates a new database but only if the database doesn't exist yet"""
 from __future__ import with_statement
 import os
+import settings
 from simblin.extensions import db
 from simblin import create_app
 
-app = create_app()
+app = create_app(settings) 
 
 with app.test_request_context():
     # The context is needed so db can access the configuration of the app
     db.create_all()
 
 print "Initialized new empty database in %s" % app.config['SQLALCHEMY_DATABASE_URI']
+
