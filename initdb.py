4bf79dfdc69b958a32ca322753470e636e302dd8
U-gfreezy-PC\gfreezy <gfreezy@gfreezy-PC.(none)>
2011-10-17 23:21:40 +0800

    init db with settings

--- initdb.py
+++ initdb.py
@@ -1,11 +1,14 @@
 """Creates a new database but only if the database doesn't exist yet"""
 from __future__ import with_statement
 import os
-import settings
+try:
+    import settings
+except ImportError:
+    settings = None
 from simblin.extensions import db
 from simblin import create_app
 
-app = create_app(settings) 
+app = create_app(settings)
 
 with app.test_request_context():
     # The context is needed so db can access the configuration of the app
